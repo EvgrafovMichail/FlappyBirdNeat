53be80536ac8edf6c81b5cb4603ec131745f8fba
EvgrafovMichal <autumnevilmen@gmail.com>
2023-05-28 19:00:44 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import os
 
 import pygame
+import neat
+
+from neat_algo import FlappyNeat
 
 from game.controllers import EnvController, ScoreController
 from game.game_objects import Bird
@@ -10,6 +13,10 @@ from game.globals.constants import (
 
 BG_IMAGE_PATH = os.path.join('..', 'data', 'sprites', 'background.png')
 BACKGROUND_IMAGE = pygame.image.load(BG_IMAGE_PATH)
+
+window = pygame.display.set_mode(
+    (GAME_FIELD_WIDTH, GAME_FIELD_HEIGHT)
+)
 
 
 def draw_window(
@@ -23,7 +30,7 @@ def draw_window(
     env_controller.draw(window)
 
 
-def main():
+def main(agent):
 
     window = pygame.display.set_mode(
         (GAME_FIELD_WIDTH, GAME_FIELD_HEIGHT)
@@ -44,28 +51,40 @@ def main():
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                bird.flap()
+        output = agent.activate((
+                bird.position.y_top_pos,
+                env_controller.neares_pipe_position.x_left_pos - bird.position.x_left_pos,
+                env_controller.neares_pipe_position.y_top_pos - bird.position.y_top_pos,
+                env_controller.neares_pipe_position.y_bottom_pos - bird.position.y_top_pos
+            ))[0]
+
+        if output > 0.5:
+            bird.flap()
 
         env_controller.move()
         bird.move()
 
         score = env_controller.get_score(bird)
 
-        if score == -1000:
+        if score == -1:
             bird = Bird()
             env_controller = EnvController()
             score_controller = ScoreController()
             score_global = 0
 
             continue
-        
-        elif score == 1:
+
+        if score == 1:
             score_global += 1
 
         draw_window(window, bird, env_controller)
-        score_controller.draw(window, score)
+        score_controller.draw(window, score_global)
         pygame.display.update()
 
 if __name__ == '__main__':
-    main()
+    config_path = '../data/neat_config_files/config_feedforward.txt'
+
+    flappy_neat = FlappyNeat(config_path, score_threshold=150)
+    winner = flappy_neat.get_winner()
+
+    main(winner)
