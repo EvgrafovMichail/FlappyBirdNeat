4abab35ae63ef1061517b89b195ab5a23240a75c
EvgrafovMichal <autumnevilmen@gmail.com>
2023-05-28 19:00:38 +0300

    update EnvController

--- src/game/controllers/env_controller.py
+++ src/game/controllers/env_controller.py
@@ -4,20 +4,22 @@ import pygame
 
 from game.game_objects import PipesPair, Floor, Bird
 from game.globals.constants import GAME_FIELD_WIDTH
+from game.game_objects.schemas import Position
 
 class EnvController:
 
+    __nearest_pipe_id: int = 0
     __pipes: List[PipesPair]
     __floor: Floor
 
-    __distance_between_pipes: int = 165
+    __distance_between_pipes: int = 180
 
     def __init__(self) -> None:
         self.__pipes = [PipesPair()]
         self.__floor = Floor()
 
     def move(self) -> None:
-        
+
         pipe_to_remove = None
 
         for pipe in self.__pipes:
@@ -34,17 +36,18 @@ class EnvController:
 
         if pipe_to_remove:
             self.__pipes.remove(pipe_to_remove)
+            self.__nearest_pipe_id = 0
 
         self.__floor.move()
 
     def get_score(self, bird: Bird) -> float:
 
         if self.__is_collided(bird):
-            return -1000
-        
+            return -1
+
         if self.__is_pipe_passed(bird):
             return 1
-        
+
         return 0.01
 
     def draw(self, window: pygame.Surface) -> None:
@@ -55,7 +58,7 @@ class EnvController:
         self.__floor.draw(window)
 
     def __is_collided(self, bird: Bird) -> bool:
-        
+
         mask_bird = bird.mask
         pos_bird = bird.position
 
@@ -64,33 +67,33 @@ class EnvController:
             mask_pipe_top, mask_pipe_bttm = pipe.mask
             pos_pipe = pipe.position
 
-            offset_top = (pos_pipe.x_left_pos - pos_bird.x_left_pos, 
+            offset_top = (pos_pipe.x_left_pos - pos_bird.x_left_pos,
                           pos_pipe.y_top_pos - int(pos_bird.y_top_pos))
-            
-            offset_bottom = (pos_pipe.x_left_pos - pos_bird.x_left_pos, 
+
+            offset_bottom = (pos_pipe.x_left_pos - pos_bird.x_left_pos,
                              pos_pipe.y_bottom_pos - int(pos_bird.y_bottom_pos))
-            
+
             collision_top = mask_bird.overlap(mask_pipe_top, offset_top)
             collision_bottom = mask_bird.overlap(mask_pipe_bttm, offset_bottom)
 
             if collision_top or collision_bottom:
                 return True
-            
+
         if pos_bird.y_bottom_pos >= self.__floor.position.y_top_pos:
             return True
-        
+
         return False
-    
+
     def __is_pipe_passed(self, bird: Bird) -> bool:
 
-        nearest_pipe_id = 0
+        nearest_pipe_id_prev = self.__nearest_pipe_id
 
         if len(self.__pipes) > 1:
-            if self.__pipes[0].position.x_left_pos < bird.position.x_left_pos:
-                nearest_pipe_id = 1
+            if self.__pipes[0].position.x_right_pos < bird.position.x_left_pos:
+                self.__nearest_pipe_id = 1
 
-        pipes_center_pos = self.__pipes[nearest_pipe_id].position.x_left_pos
-        pipes_center_pos += self.__pipes[nearest_pipe_id].position.x_right_pos
-        pipes_center_pos /= 2
-
-        return pipes_center_pos < bird.position.x_left_pos
+        return nearest_pipe_id_prev != self.__nearest_pipe_id
+    
+    @property
+    def neares_pipe_position(self) -> Position:
+        return self.__pipes[self.__nearest_pipe_id].position
