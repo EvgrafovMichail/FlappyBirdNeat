80d954ba24a90385fc93d4543cf3d412f91dd6da
EvgrafovMichal <autumnevilmen@gmail.com>
2023-05-30 20:46:35 +0300

    add comments into constants.py in controllers folder

--- src/game/controllers/constants.py
+++ src/game/controllers/constants.py
@@ -1,8 +1,13 @@
 import os
 
+################################################################################
+#                                PATHES TO IMAGES                              #
+################################################################################
 
+# path to background image
 BG_IMAGE_PATH = os.path.join('..', 'data', 'sprites', 'background.png')
 
+# pathes to digit images
 PATH_TO_DIGIT_FOLDER = os.path.join('..', 'data', 'sprites', 'digits')
 PATHES_TO_DIGIT_IMGS = [
     os.path.join(PATH_TO_DIGIT_FOLDER, f'{i}.png')
